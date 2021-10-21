c1985d7698c020f670922f476afb69884fbce8d8
hama123yaxi <92339868+hama123yaxi@users.noreply.github.com>
2021-10-22 02:41:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('OTAwODg5NDc5MjMxOTcxMzI4.YXH4tQ.aXX9zg-XYwXhIqmBn0LlnVf8ENs');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
